731cbd4774ce02bc98bb5886bc47e05403a935f4
RipplyPlane420 <juanpablindl304@gmail.com>
2026-07-22 19:41:47 -0600

    Version 1.11.0

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        hermosillo: resolve(__dirname, 'sucursales/hermosillo.html'),
+        guadalajara: resolve(__dirname, 'sucursales/guadalajara.html'),
+        aguascalientes: resolve(__dirname, 'sucursales/aguascalientes.html'),
+      },
+    },
   },
   server: {
     port: 3000
   }
 });
+
